279b79d188d1d8c28b2f7a5c2ace1fd8a93777d9
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2021-12-14 06:18:37 +0200

    Update dayo2p1.py

--- day02/dayo2p1.py
+++ day02/dayo2p1.py
@@ -1,5 +1,4 @@
-horip = 0
-downp = 0
+
 
 pos = ["forward 5",
 "down 5",
@@ -8,13 +7,21 @@ pos = ["forward 5",
 "down 8",
 "forward 2"]
 
-for i in pos:
-    if i[0] == "f":
-        horip += int(i[-1])
-    elif i[0] == "u":
-        downp -= int(i[-1])
-    else:
-        downp += int(i[-1])
+
+def finddepth(lista):
+    horip = 0
+    downp = 0
+    for i in lista:
+        if i[0] == "f":
+            horip += int(i[-1])
+        elif i[0] == "u":
+            downp -= int(i[-1])
+        else:
+            downp += int(i[-1])
+    return horip*downp
 
 
-print(horip*downp)
+with open("day02/day02p1.txt") as f:
+    raw = f.read()
+    input = [x for x in raw.split('\n')]
+    print(finddepth(input))
