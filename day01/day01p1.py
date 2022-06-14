5a48f6b643a5d8dcce45c4d7bd378b223f895da6
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2022-06-14 19:20:13 +0300

    Update day01p1.py

--- day01/day01p1.py
+++ day01/day01p1.py
@@ -18,11 +18,29 @@ def countinc(lista):
             incretimes += 1
     return incretimes
 
+def triplecounter(n):
+    counter = 0
+    psum = 0
+    sum1 = 0
+    for i in range(len(n)):
+        for i in range(i,i+3):
+            if i+1 >= len(n):
+                break
+            else:
+                sum1+= n[i+1]
+        if sum1 > psum:
+            counter+=1
+        psum = sum1
+        sum1 = 0
+    return counter
+
 
 print(countinc(n))
+print(triplecounter(n))
 
 with open("day01/day01p1.txt") as f:
     raw = f.read()
     input = [int(x) for x in raw.split('\n')]
     print(input)
     print(countinc(input))
+    print(triplecounter(input))
