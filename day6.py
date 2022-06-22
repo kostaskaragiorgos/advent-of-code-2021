bbcba2637e00b060d517aba321042a924ea011bb
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2022-06-22 19:28:22 +0300

    Update day6.py

--- day6.py
+++ day6.py
@@ -3,4 +3,13 @@ with open("day6.txt") as f:
     initstate =f.read()
     initstate = initstate.split(",")
     initstate = [int(inits) for inits in initstate]
-    print(initstate)
+
+for _ in range(80):
+    for i in range(len(initstate)):
+        if initstate[i] == 0:
+            initstate[i] = 6
+            initstate.append(8)
+        else:
+            initstate[i] -= 1
+
+print(len(initstate))
